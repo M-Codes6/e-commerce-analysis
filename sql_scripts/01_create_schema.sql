5dcefd0cf8dd131c30813f1fec14e18c33997442
Muzamil Mushtaq <M-Codes6@users.noreply.github.com>
2026-04-28 21:58:57 +0530

    Step 1: Established database schema with Customers and Orders tables

---
+++ sql_scripts/01_create_schema.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE ecommerce_db;
+
+USE ecommerce_db;
+
+CREATE TABLE customers (
+
+customer_id INT PRIMARY KEY AUTO_INCREMENT,
+first_name VARCHAR(60),
+last_name VARCHAR(60),
+email VARCHAR(100) UNIQUE,
+signup_data DATE
+);
+
+CREATE TABLE orders (
+order_id INT PRIMARY KEY AUTO_INCREMENT,
+customer_id INT,
+order_data DATE,
+total_amount DECIMAL(10, 2),
+FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+
+);
