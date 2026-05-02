6995bbb35fb08ab0f40152f86deb58b6ab9adff0
Muzamil Mushtaq <M-Codes6@users.noreply.github.com>
2026-05-02 14:57:00 +0530

    Update Business insights to LEFt JOIN

--- sql_scripts/03_business_insights.sql
+++ sql_scripts/03_business_insights.sql
@@ -1,6 +1,13 @@
 SELECT c.first_name, c.last_name,
+COUNT(o.order_id) AS total_orders,
 SUM(o.total_amount) AS total_spent
 FROM customers c 
 JOIN orders o ON c.customer_id = o.customer_id
 GROUP BY c.customer_id
 ORDER BY total_spent DESC;
+
+
+SELECT c.first_name, c.last_name, c.email
+FROM customers c
+LEFt JOIN orders o ON c.customer_id = o.customer_id
+WHERE o.order_id IS NULL;
