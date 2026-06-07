1dc7b0884f4b20bd9f4005f9d6bf3a59e5e182db
Muzamil Mushtaq <M-Codes6@users.noreply.github.com>
2026-06-07 20:28:01 +0530

    code fixed

--- sql_scripts/02_data_seeding.sql
+++ sql_scripts/02_data_seeding.sql
@@ -160,6 +160,8 @@ INSERT INTO products (product_name, category_id, price) VALUES
 ('HP Laptop',            5,   400.00),
  
  
+
+ 
 -- New records (12–30)
 ('iPad Pro 12.9"',       6,  1100.00),
 ('Samsung Tab S9',       6,   850.00),
